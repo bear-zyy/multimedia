88194a075e67c1c5e72272723b7bb684a424e0fa
bear-zyy <13520592875@163.com>
2018-10-30 18:15:38 +0800

    异步

--- GCD/SemaphoreViewController.m
+++ GCD/SemaphoreViewController.m
@@ -24,9 +24,9 @@
     [but setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [but addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:but];
-
+//
 //    [self syncMethod];
-//    [self asyncMethod];
+    [self asyncMethod];
 //    [self group];
     
 }
@@ -39,42 +39,42 @@
     
     
     ///组的一种方式
-    dispatch_group_async(group, queue, ^{
-        NSLog(@"1");
-    });
-    dispatch_group_async(group, queue, ^{
-        NSLog(@"2");
-    });
-    dispatch_group_async(group, queue, ^{
-        
-        NSLog(@"3");
-    });
-   
-    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
-        NSLog(@"123");
-    });
+//    dispatch_group_async(group, queue, ^{
+//        NSLog(@"1");
+//    });
+//    dispatch_group_async(group, queue, ^{
+//        NSLog(@"2");
+//    });
+//    dispatch_group_async(group, queue, ^{
+//
+//        NSLog(@"3");
+//    });
+//
+//    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+//        NSLog(@"123");
+//    });
     
 //    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
     
     //组的另一种方式
-//    dispatch_group_enter(group);
-//    dispatch_async(queue, ^{
-//        NSLog(@"11");
-//        dispatch_group_leave(group);
-//    });
-//    dispatch_group_enter(group);
-//    dispatch_async(queue, ^{
-//        NSLog(@"22");
-//        dispatch_group_leave(group);
-//    });
-//    dispatch_group_enter(group);
-//    dispatch_async(queue, ^{
-//        NSLog(@"33");
-//        dispatch_group_leave(group);
-//    });
-//    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
-//        NSLog(@"112233");
-//    });
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        NSLog(@"11");
+        dispatch_group_leave(group);
+    });
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        NSLog(@"22");
+        dispatch_group_leave(group);
+    });
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        NSLog(@"33");
+        dispatch_group_leave(group);
+    });
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        NSLog(@"112233");
+    });
 }
 
 
@@ -85,20 +85,29 @@
 -(void)syncMethod{
     //并发队列
     
-//    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0); //GDC默认提供全局并发并列
-//
-//    // 其实不管是串行队列还是并发队列都是要创建的，但是GCD提供了并发队列，就不需要创建了，
-//    dispatch_sync(queue, ^{
-//        NSLog(@"1");
-//    });
-//    dispatch_sync(queue, ^{
-//        NSLog(@"2");
-//    });
-//
-//    dispatch_sync(queue, ^{
-//        NSLog(@"3");
-//    });
- 
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0); //GDC默认提供全局并发并列
+
+    // 其实不管是串行队列还是并发队列都是要创建的，但是GCD提供了并发队列，就不需要创建了，
+    dispatch_sync(queue, ^{
+        NSLog(@"1");
+    });
+    
+    dispatch_sync(queue, ^{
+        NSLog(@"2");
+    });
+
+    dispatch_sync(queue, ^{
+        NSLog(@"3");
+    });
+    
+    dispatch_sync(queue, ^{
+        NSLog(@"4");
+    });
+    
+    dispatch_sync(queue, ^{
+        NSLog(@"5");
+    });
+
 //    NSLog(@"currentThread---%@",[NSThread currentThread]);  // 打印当前线程
     /*
      其实 并发队列中添加多个同步任务， 其实同步任务也都是并发执行了， 但是由于同步任务不具备开启线程的能力，所以只能等主线程， 所以也导致任务都是串行执行的，即使是在并发队列中
@@ -125,50 +134,80 @@
     
 
 //    dispatch_queue_t queue = dispatch_get_main_queue();
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-
-     dispatch_async(queue, ^{
-         // 追加任务1
-         for (int i = 0; i < 2; ++i) {
-             [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
-             NSLog(@"1---%@",[NSThread currentThread]);      // 打印当前线程
-         }
-     });
-
-    dispatch_async(queue, ^{
-        // 追加任务2
-        for (int i = 0; i < 2; ++i) {
-            [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
-            NSLog(@"2---%@",[NSThread currentThread]);      // 打印当前线程
-        }
-    });
-
-    dispatch_async(queue, ^{
-        // 追加任务3
-        for (int i = 0; i < 2; ++i) {
-            [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
-            NSLog(@"3---%@",[NSThread currentThread]);      // 打印当前线程
-        }
-    });
-
-    NSLog(@"asyncMain---end");
+//    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//
+//     dispatch_async(queue, ^{
+//         // 追加任务1
+//         for (int i = 0; i < 2; ++i) {
+//             [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
+//             NSLog(@"1---%@",[NSThread currentThread]);      // 打印当前线程
+//         }
+//     });
+//
+//    dispatch_async(queue, ^{
+//        // 追加任务2
+//        for (int i = 0; i < 2; ++i) {
+//            [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
+//            NSLog(@"2---%@",[NSThread currentThread]);      // 打印当前线程
+//        }
+//    });
+//
+//    dispatch_async(queue, ^{
+//        // 追加任务3
+//        for (int i = 0; i < 2; ++i) {
+//            [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
+//            NSLog(@"3---%@",[NSThread currentThread]);      // 打印当前线程
+//        }
+//    });
+//
+//    NSLog(@"asyncMain---end");
     
     
 }
 //异步
 -(void)asyncMethod{
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        for (int i=0; i< 10; i++) {
-            NSLog(@"%d" , i);
-        }
-    });
-    
+    //这是获取一个全局的并发队列
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     
-    dispatch_apply(10, queue, ^(size_t index) {
-        NSLog(@"10");
+    dispatch_async(queue, ^{
+        NSLog(@"1");
     });
+    
+    dispatch_async(queue, ^{
+        NSLog(@"2");
+    });
+    
+    //这个是创建一个并发队列
+    dispatch_queue_t queue2 = dispatch_queue_create("queue2", DISPATCH_QUEUE_CONCURRENT);
+    
+    dispatch_async(queue2, ^{
+        NSLog(@"3");
+    });
+    
+    dispatch_async(queue2, ^{
+        NSLog(@"4");
+    });
+    
+    dispatch_async(queue2, ^{
+        NSLog(@"5");
+    });
+    
+    dispatch_async(queue2, ^{
+        NSLog(@"6");
+    });
+    // 并发队列允许任务并发执行   就要看任务是否有开启线程的能力了
+    
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//        for (int i=0; i< 10; i++) {
+//            NSLog(@"%d" , i);
+//        }
+//    });
+//
+//    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//
+//    dispatch_apply(10, queue, ^(size_t index) {
+//        NSLog(@"10");
+//    });
 }
 
 
