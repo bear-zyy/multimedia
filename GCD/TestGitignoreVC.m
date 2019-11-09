414077ac83fcb9cd678e1ff9297cb49ac9ea0521
bear <13520592875@163.com>
2019-11-09 16:22:20 +0800

    修改.gitignore

---
+++ GCD/TestGitignoreVC.m
@@ -0,0 +1,32 @@
+//
+//  TestGitignoreVC.m
+//  GCD
+//
+//  Created by 心悦互动1 on 2019/11/9.
+//  Copyright © 2019 张源远. All rights reserved.
+//
+
+#import "TestGitignoreVC.h"
+
+@interface TestGitignoreVC ()
+
+@end
+
+@implementation TestGitignoreVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
